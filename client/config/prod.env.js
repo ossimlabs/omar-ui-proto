dce0a44395861692a6d4e0bc7d40aded108225d2
Matthew Esterberg <matthewesterberg@mxradmins-MacBook-Pro.local>
2019-08-22 11:57:44 -0400

    First commit.

---
+++ client/config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  SERVER_URL: '"https://omar-dev.ossim.io/omar-ui-proto"'
+}
---
+++ server/grails-app/init/omar/ui/proto/BootStrap.groovy
@@ -0,0 +1,9 @@
+package omar.ui.proto
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
