b456ac1302edcba83552d64cec3030d07e3fa12d
Matthew Esterberg <matthewesterberg@mxradmins-MBP.fios-router.home>
2019-08-25 17:57:09 -0400

    saving progress.  main aplsah page is coming along.  Restyle everything and got the layout to cooperate.

--- client/src/components/MainPageSplash/MainPageSplash.vue
+++ client/src/components/MainPageSplash/MainPageSplash.vue
@@ -1,36 +1,35 @@
 <template>
 <b-container>
-  <b-row>
-    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-      <h1 class="display-4">Image Discovery and Analysis</h1>
-    </div>
+  <b-row align-h="center mt-5">
+    <b-col>
+      <h1 class="text-center mb-4">Image Discovery and Analysis</h1>
+    </b-col>
   </b-row>
 
   <b-row>
-    <b-card-group columns>
+    <b-card-group deck>
       <b-card
         v-for="(card, index) in cards"
         :key="index"
-        class="ma-2 pa-2 custom-card text-center"
+        class="m-4 p-2 shadow custom-card text-center"
       >
-        <i :class="`fas fa-${card.icon}`" class="fa-7x mb-3" ></i>
-        <b-card-title>
+        <i :class="`fas fa-${card.icon}`" :style="{color:card.color}" class="fa-7x mb-3"></i>
+        <b-card-title class="text-capitalize">
           {{ card.title }}
         </b-card-title>
 
-        <div class="overlay">
-          <b-card-text>
+        <div class="overlay shadow-lg">
+          <b-card-text class="p-3">
             {{ card.desc }}
           </b-card-text>
         </div>
       </b-card>
     </b-card-group>
-
   </b-row>
 
-  <b-row align-h="center" class="mt-5">
+  <b-row align-h="center" class="m-5">
     <b-alert show text dismissible fade class="text-center">
-      <h4 class="alert-heading">New federation capability!</h4>
+      <h4 class="alert-heading text-capitalize">new federation capability!</h4>
       <p>Select an O2 server from the new dropdown box located above the result cards list!</p>
     </b-alert>
   </b-row>
@@ -48,27 +47,31 @@ export default {
         {
           title: 'Search',
           icon: 'search',
-          color: '',
+          color: 'black',
           desc: 'Search and discover various types of imagery. Use this as a starting point for filtering, sorting, and assembling imagery into a collection'
         },
         {
           title: 'TLV',
           icon: 'clock',
+          color: 'blue',
           desc: 'An on-demand imagery flipbook'
         },
         {
           title: 'KML',
           icon: 'map',
+          color: 'orange',
           desc: 'download a kml of the last [10] images acquired.'
         },
         {
-          title: 'GeoRss',
+          title: 'GeoRSS',
           icon: 'wifi',
+          color: 'green',
           desc: 'create a georss feed of images.'
         },
         {
           title: 'Image upload',
           icon: 'upload',
+          color: 'black',
           desc: 'a place to upload images.'
         }
       ]
@@ -86,7 +89,6 @@ export default {
 <style scoped>
 .custom-card:hover .overlay{
   opacity: 0.9;
-  z-index:9999;
 }
 .overlay {
   position: absolute;
@@ -97,14 +99,8 @@ export default {
   height: 100%;
   width: 100%;
   opacity: 0;
-  /*border-radius: 5px;*/
   transition: .5s ease;
-  background-color: rgba(0,0,0,.03);
-}
-
-.custom-card {
-  display: inline-block;
-  z-index: 1;
+  background-color: #f1f1f1;
 }
 
 .card-deck .card {
