8a26b699e822bc709ccdd99c582875d871cdbb78
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-09-04 02:03:44 -0400

    first push with micronaut and vue.

--- client/config/prod.env.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  SERVER_URL: '"https://omar-dev.ossim.io/omar-ui-proto"'
-}
--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,10 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './App.vue'
 import router from './router'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-
-import '@fortawesome/fontawesome-free/css/all.css' // Ensure you are using css-loader
-
-
-
 Vue.config.productionTip = false
-Vue.use(BootstrapVue, {
-  iconfont: 'fa'
-})
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  render: h => h(App),
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
