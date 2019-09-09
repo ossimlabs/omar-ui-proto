7856e98c02b12e72974cd03ab6f31dd75fe88270
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-09-09 10:38:48 -0400

    Removed some no longer needed code.  Going to test turning history:on, on dev.  saving progress.

--- client/src/router.js
+++ client/src/router.js
@@ -6,8 +6,6 @@ Vue.use(Router)
 
 export default new Router({
   // mode: 'history',
-  // base: process.env.BASE_URL + '/omar-ui-proto',
-  // base: '/omar-ui-proto',
   routes: [
     {
       path: '/',
--- omar-ui-proto/src/main/java/example/micronaut/HelloController.java
+++ omar-ui-proto/src/main/java/example/micronaut/HelloController.java
@@ -1,27 +1,13 @@
 package example.micronaut;
 
 import io.micronaut.http.HttpResponse;
-import io.micronaut.http.HttpRequest;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
-import io.micronaut.core.io.ResourceResolver;
-import io.micronaut.http.server.types.files.StreamedFile;
 
 @Controller("/omar-ui-proto")
 public class HelloController {
-//    @Inject
-//    ResourceResolver res;
-//
-//    @Get("/{[path:[^\\.]*}")
-//    @Secured(SecurityRule.IS_ANONYMOUS)
-//    @Produces(MediaType.TEXT_HTML)
-//
-//    public HttpResponse<?> refresh(HttpRequest<?> request) {
-//        StreamedFile indexFile = new StreamedFile(res.getResource("classpath:public/index.html").get());
-//        return HttpResponse.ok(indexFile);
-//    }
     @Produces(MediaType.TEXT_PLAIN)
     @Get("/{name}")
     HttpResponse<String> hello(String name) {
