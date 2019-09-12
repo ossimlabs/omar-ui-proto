acdc15f308b401c2e06680f99aaf1d60c5032340
knaquin <kevin.naquin@digitalglobe.com>
2019-09-12 14:43:51 -0400

    Testing

--- omar-ui-proto/src/main/java/example/micronaut/HelloController.java
+++ omar-ui-proto/src/main/java/example/micronaut/HelloController.java
@@ -1,17 +1,21 @@
 package example.micronaut;
 
-import io.micronaut.http.HttpResponse;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Produces;
 
+
 @Controller("/omar-ui-proto")
 public class HelloController {
-    @Produces(MediaType.TEXT_PLAIN)
-    @Get("/{name}")
-    HttpResponse<String> hello(String name) {
-        return HttpResponse.ok("Hello SCOTTY!, " + name + "!");
-    }
 
+    @Value("${base-url}")
+    String baseUrl;
+
+    @Get("/config")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String index() {
+        return baseUrl;
+    }
 }
