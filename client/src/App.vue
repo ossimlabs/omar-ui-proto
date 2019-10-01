e25e83679b7f87deac8fa0ce6ecd0769f3046476
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-10-01 17:51:01 -0400

    fixed small bug on results page where adding chips would create a substantial amount of padding.

--- client/src/App.vue
+++ client/src/App.vue
@@ -36,33 +36,13 @@
         <v-spacer></v-spacer>
 
         <UserProfileSplash></UserProfileSplash>
-
-
-
       </v-app-bar>
+
       <!-- Content Area -->
       <v-content>
-        <v-container fluid fill-height pa-0>
-          <v-row no-gutters>
-            <!--<v-col :class="{ fixed: isFixed }" class="pt-0 mt-2 mx-2">
-              <v-expansion-panels class="elevation-5">
-                <v-expansion-panel >
-                  <v-expansion-panel-header>Filters</v-expansion-panel-header>
-                  <v-expansion-panel-content>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                  </v-expansion-panel-content>
-                </v-expansion-panel>
-              </v-expansion-panels>
-            </v-col>-->
-
-            <transition name="page-fade" mode="out-in">
-              <v-col cols="12">
-                <router-view></router-view>
-              </v-col>
-            </transition>
-          </v-row>
-
-        </v-container>
+        <transition name="page-fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
       </v-content>
 
       <!-- Footer -->
@@ -91,32 +71,14 @@ export default {
       }
     },
     magicword: null,
-    isFixed: false,
     //
   }),
-  created () {
-    // window.addEventListener('scroll', this.handleScroll);
-  },
-  destroyed () {
-    // window.removeEventListener('scroll', this.handleScroll);
-  },
+  created () {},
+  destroyed () {},
   mounted () {},
   computed: {},
   watch: {},
   methods: {
-    handleScroll (event) {
-      // let stickyTop = this.$refs.filterBar.offset().top
-
-      console.log('window', window)
-      if (window.scrollY >= 19) {
-        this.isFixed = true
-        console.log('at the top!')
-      } else {
-        this.isFixed = false
-      }
-      // console.log('event', event)
-      // Any code to be executed when the window is scrolled
-    },
     doMagic(magicword) {
       this.$store.commit('addFilter', {category: 'word', type: 'magicword', value: magicword})
       this.magicword = null
