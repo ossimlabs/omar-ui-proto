9900cf354a964d887618e3db4ea968c6864fd6d7
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-10-03 15:04:19 -0400

    found npm repo that resolves the FF30 bug where it wouldnt load images.  IntersectionObserver.  Still need to work on, or change, how FF30 handles the keyword/ID filter.  Currently you are unable to type with in it because it sees it as a button instead of a input box.

--- client/src/main.js
+++ client/src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import vuetify from './plugins/vuetify';
 import store from './store'
 import '@babel/polyfill'
+import 'intersection-observer'
 
 Vue.config.productionTip = false
 
