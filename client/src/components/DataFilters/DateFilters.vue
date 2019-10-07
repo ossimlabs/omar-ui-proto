05722ec5a8b02970feccbc6330beaba0ded728c2
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-10-07 15:40:21 -0400

    added ability to manually type in date.  Still lacks error correction and validation.  Working on adding ingest & acquisition options.

--- client/src/components/DataFilters/DateFilters.vue
+++ client/src/components/DataFilters/DateFilters.vue
@@ -15,7 +15,7 @@
       <template v-slot:activator="{ on }">
         <v-combobox
           v-model="dates"
-          :label="'Choose ' + date_type "
+          label="Date"
           prepend-icon="fa-calendar-alt"
           readonly
           v-on="on"
@@ -30,28 +30,35 @@
         v-model="dates"
         :multiple="multiple"
         :range="range"
+        value="dates"
       >
         <v-container style="" class="ma-0 py-0">
           <v-row justify="center" class="" no-gutters>
             <v-col class="py-0 my-0">
-              <span>Range {{ range }} </span> <br/>
-              <span>Multiple {{ multiple }}</span> <br />
-              <span>dates: {{ dates }}</span>
+              <span>{{ date_type }}: {{ dates }}</span>
             </v-col>
           </v-row>
           <v-row justify="center">
             <v-col cols="10">
+              <v-text-field
+                  v-model="exact_date"
+                  v-show="calendar_type === 0"
+                  label="Or type exact date"
+                  placeholder="Format: YYYY-MM-DD"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="10">
               <v-btn-toggle v-model="calendar_type">
                 <v-btn class="ma-0"><v-icon>fa-dot-circle</v-icon></v-btn>
                 <v-btn class="ma-0"><v-icon>fa-exchange-alt</v-icon></v-btn>
-                <v-btn class="ma-0"><v-icon>fa-arrows-alt-h</v-icon></v-btn>
+                <v-btn class="ma-0"><v-icon disabled>fa-arrows-alt-h</v-icon></v-btn>
                 <v-btn class="ma-0"><v-icon>fa-braille</v-icon></v-btn>
               </v-btn-toggle>
             </v-col>
           </v-row>
 
           <v-row justify="center">
-            <v-col>
+            <v-col cols="10">
               <v-btn text color="primary" @click="menu = false">Cancel</v-btn>
               <v-btn text color="primary" @click="$refs.menu.save(), addDateFilter(dates)">Add Filter</v-btn>
             </v-col>
@@ -64,6 +71,8 @@
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
   name: 'DateFilters',
   props: {},
@@ -71,10 +80,10 @@ export default {
   data: () => ({
     menu: false,
     calendar_type: 0,
-    date_type: null,
+    date_type: 'exact date',
     today: ['2019-10-07'],
-    dates: '2019-10-07',
-    // dates: ['2018-09-15', '2018-09-20'],
+    exact_date: null,
+    dates: moment().format('YYYY-MM-DD'),
     multiple: false, exact:true, range: false
     // 
   }),
@@ -89,11 +98,11 @@ export default {
           : val === 1 ? 'range of dates'
           : val === 2 ? 'range with starting point'
           : val === 3 ? 'multiple dates'
-          : 'exact'
+          : 'exact date'
       }
       this.date_type = determineDateType(newVal)
       this.switchCalendarTo(newVal)
-      console.log('date_type', determineDateType(newVal))
+
     }
   },
   methods: {
@@ -101,18 +110,21 @@ export default {
 
       // TODO make this better
       if (index_type === 0) {
-        this.range = this.multiple = false
         this.dates = '2019-10-07'
+        this.range = this.multiple = false
       } else if (index_type === 1) {
+        this.dates = ['2019-10-07']
         this.range = true
         this.multiple = false
-        this.dates = ['2019-10-07']
       } else if (index_type === 2) {
 
       } else if (index_type === 3) {
+        this.dates = ['2019-10-07']
         this.multiple = true
         this.range = false
-        this.dates = ['2019-10-07']
+      } else {
+        this.dates = '2019-10-07'
+        this.range = this.multiple = false
       }
 
     },
