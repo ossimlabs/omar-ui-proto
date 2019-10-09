d6779feb1b3ddb57682baefe5e0e2e45b47657e5
Matthew Esterberg <matthew.esterberg@maxar.com>
2019-10-09 10:14:49 -0400

    saving progress.  removed some extra markup.  properly aligned new time boxes.  removed calendar border to make additional options box look a bit nicer.

--- client/src/components/DataFilters/DateFilters.vue
+++ client/src/components/DataFilters/DateFilters.vue
@@ -1,14 +1,11 @@
 <template>
   <v-col cols="10" class="">
-
-
     <v-menu
       ref="menu"
       v-model="menu"
       :close-on-content-click="false"
       transition="scale-transition"
       offset-y
-      style="width:100px"
     >
       <!-- menu template -->
       <template v-slot:activator="{ on }">
@@ -25,122 +22,119 @@
 
       <!-- date picker -->
       <v-date-picker
-        class="ma-0 py-0 custom-width" scrollable no-title reactive
+        class="custom-width remove-shadow elevation-0" scrollable no-title reactive
         v-model="dates"
         :multiple="multiple"
         :range="range"
         value="dates"
         color="primary"
         :disabled="disable_datepicker"
-        :class="{'hide': disable_datepicker}"
       >
-        <v-container style="" class="ma-0 py-0">
-          <v-row justify="center">
-
-            <!-- Calendar Type Buttons -->
-            <!-- TODO make this a directive/component -->
-            <v-col cols="10" class="mb-0 pb-0">
-              <v-btn-toggle v-model="calendar_type" color="primary" active-class="green-highlight">
-                <v-tooltip top>
-                  <template v-slot:activator="{ on }">
-                    <v-btn class="ma-0" v-on="on"><v-icon>fa-dot-circle</v-icon></v-btn>
-                  </template>
-                  <span>Exact Date / Time</span>
-                </v-tooltip>
-
-                <v-tooltip top>
-                  <template v-slot:activator="{ on }">
-                    <v-btn class="ma-0" v-on="on"><v-icon>fa-exchange-alt</v-icon></v-btn>
-                  </template>
-                  <span>Date Range</span>
-                </v-tooltip>
-
-                <v-tooltip top>
-                  <template v-slot:activator="{ on }">
-                    <v-btn class="ma-0" v-on="on" disabled><v-icon>fa-arrows-alt-h</v-icon></v-btn>
-                  </template>
-                  <span>Date Within</span>
-                </v-tooltip>
-
-                <v-tooltip top>
-                  <template v-slot:activator="{ on }">
-                    <v-btn class="ma-0" v-on="on"><v-icon>fa-braille</v-icon></v-btn>
-                  </template>
-                  <span>Multiple Dates</span>
-                </v-tooltip>
-              </v-btn-toggle>
-            </v-col>
-
-            <!-- Exact Date !Optional! -->
-            <v-col cols="10" class="mb-0 mt-2 pt-2 py-0">
-              <transition name="page-fade" mode="out-in">
-                <v-text-field
-                  v-model="user_generated_date_string"
-                  v-show="calendar_type === 0"
-                  label="Type exact date"
-                  placeholder="YYYY-MM-DD"
-                ></v-text-field>
-              </transition>
-            </v-col>
-
-            <!-- Time -->
-            <!-- NPM packaged time pickers.  Smaller profile, easier to use -->
-            <v-col cols="10" class="mb-0 mt-2 py-0 align-content-center">
-              <transition-group name="page-fade" mode="out-in">
-                <vue-timepicker
-                  class="mb-2"
-                  v-show="calendar_type === 0 || calendar_type == null"
-                  key="start_time"
-                  placeholder="Start Time"
-                  format="HH:mm:ss"></vue-timepicker>
-                <vue-timepicker
-                  v-show="calendar_type === 0 || calendar_type == null"
-                  key="end_time"
-                  placeholder="End Time"
-                  format="HH:mm:ss"></vue-timepicker>
-              </transition-group>
-            </v-col>
-
-            <!-- Ingest & Acquisition -->
-            <v-col cols="10" class="my-0 py-0">
-              <v-row justify="space-between" no-gutters>
-                <v-checkbox
-                  color="primary"
-                  v-model="ingest_date"
-                  label="Ingest"
-                ></v-checkbox>
-                <v-checkbox
-                  color="primary"
-                  v-model="acquisition_date"
-                  label="Acquisition"
-                ></v-checkbox>
-              </v-row>
-            </v-col>
-
-          </v-row>
-
-          <!-- Action Buttons -->
-          <v-row justify="center">
-            <v-col cols="6">
-              <v-btn block elevation="4" color="error" @click="menu = false">Cancel</v-btn>
-            </v-col>
-            <v-col cols="6">
-              <v-btn block  elevation="4" color="primary" @click="$refs.menu.save(), addDateFilter(dates)">Add Filter</v-btn>
-            </v-col>
-          </v-row>
-
-          <!-- Debug -->
-          <v-row justify="center" class="" no-gutters>
-            <v-col class="py-0 my-0">
-              <h4> debugging: </h4>
-              <span>{{ date_type }}: {{ dates }}</span> <br />
-              <span>calendar_type {{ calendar_type }}</span>
-            </v-col>
-          </v-row>
-
-        </v-container>
-
       </v-date-picker>
+
+
+      <v-container class="date-filters-bg py-2">
+        <!-- Calendar Type Buttons -->
+        <!-- TODO make this a directive/component -->
+        <v-row justify="center" class="mb-2 ">
+          <v-btn-toggle v-model="calendar_type" color="primary" active-class="green-highlight">
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn class="ma-0" v-on="on"><v-icon>fa-dot-circle</v-icon></v-btn>
+              </template>
+              <span>Exact Date / Time</span>
+            </v-tooltip>
+
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn class="ma-0" v-on="on"><v-icon>fa-exchange-alt</v-icon></v-btn>
+              </template>
+              <span>Date Range</span>
+            </v-tooltip>
+
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn class="ma-0" v-on="on" disabled><v-icon>fa-arrows-alt-h</v-icon></v-btn>
+              </template>
+              <span>Date Within</span>
+            </v-tooltip>
+
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn class="ma-0" v-on="on"><v-icon>fa-braille</v-icon></v-btn>
+              </template>
+              <span>Multiple Dates</span>
+            </v-tooltip>
+          </v-btn-toggle>
+        </v-row>
+
+        <!-- Exact Date !Optional! -->
+        <v-row justify="space-around">
+          <v-col cols="8" class="mb-0 mt-2 pt-2 py-0">
+            <transition name="page-fade" mode="out-in">
+              <v-text-field
+                v-model="user_generated_date_string"
+                v-show="calendar_type === 0"
+                label="Type exact date"
+                placeholder="YYYY-MM-DD"
+              ></v-text-field>
+            </transition>
+          </v-col>
+        </v-row>
+
+        <!-- Time -->
+        <!-- NPM packaged time pickers.  Smaller profile, easier to use -->
+        <v-row justify="space-around" >
+          <transition-group name="page-fade" mode="out-in">
+            <vue-timepicker
+              class="mr-2"
+              v-show="calendar_type === 0 || calendar_type == null"
+              key="start_time"
+              placeholder="Start Time"
+              format="HH:mm:ss"></vue-timepicker>
+            <vue-timepicker
+              v-show="calendar_type === 0 || calendar_type == null"
+              key="end_time"
+              placeholder="End Time"
+              format="HH:mm:ss"></vue-timepicker>
+          </transition-group>
+        </v-row>
+
+        <!-- Ingest & Acquisition -->
+        <v-row justify="space-around">
+          <v-checkbox
+            color="primary"
+            v-model="ingest_date"
+            label="Ingest"
+          ></v-checkbox>
+          <v-checkbox
+            color="primary"
+            v-model="acquisition_date"
+            label="Acquisition"
+          ></v-checkbox>
+        </v-row>
+
+        <!-- Action Buttons -->
+        <v-row justify="center">
+          <v-col cols="6">
+            <v-btn block elevation="4" color="error" @click="menu = false">Cancel</v-btn>
+          </v-col>
+          <v-col cols="6">
+            <v-btn block  elevation="4" color="primary" @click="$refs.menu.save(), addDateFilter(dates)">Add Filter</v-btn>
+          </v-col>
+        </v-row>
+
+        <!-- Debug -->
+        <v-row justify="center" class="" no-gutters>
+          <v-col class="py-0 my-0">
+            <h4> debugging: </h4>
+            <span>{{ date_type }}: {{ dates }}</span> <br />
+            <span>calendar_type {{ calendar_type }}</span>
+          </v-col>
+        </v-row>
+
+      </v-container>
+
     </v-menu>
   </v-col>
 </template>
@@ -169,10 +163,7 @@ export default {
   }),
   created () {},
   destroyed () {},
-  mounted () {
-    console.log('this $refs', this.$refs.menu.$refs)
-    console.log('this $vuetify', this.$vuetify)
-  },
+  mounted () {},
   computed: {},
   watch: {
     calendar_type: function(newVal) {
@@ -226,11 +217,12 @@ export default {
 }
 </script>
 <style scoped>
-.hide {
-  display: white;
+.date-filters-bg {
+  background-color: #424242;
 }
+
 .custom-width {
-  width: 300px;
+  width: 350px;
 }
 .green-highlight:before {
   opacity: .85 !important;
@@ -244,6 +236,17 @@ export default {
   opacity: 0;
   margin-top: -100px;
 }
+
+
+.datepicker-fade-enter-active, .datepicker-fade-leave-active {
+  transition: all .3s ease-in-out;
+}
+.datepicker-fade-enter, .datepicker-fade-leave-to
+  /* .page-fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+  margin-top: -100px;
+}
+
 </style>
 
 <style>
